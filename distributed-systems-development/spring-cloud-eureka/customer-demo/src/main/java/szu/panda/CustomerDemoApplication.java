926d7866443b445a7d6401b740cdedbb32fefccc
Vander1991 <398284823@qq.com>
2020-03-08 15:07:10 +0800

    Ribbon 负载均衡：修改CustomerDemoApplication、CustomerController内容

--- distributed-systems-development/spring-cloud-eureka/customer-demo/src/main/java/szu/panda/CustomerDemoApplication.java
+++ distributed-systems-development/spring-cloud-eureka/customer-demo/src/main/java/szu/panda/CustomerDemoApplication.java
@@ -1,14 +1,21 @@
 package szu.panda;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@RibbonClients(
+    @RibbonClient(value ="hello-server")
+)
 public class CustomerDemoApplication {
 
     public static void main(String[] args) {
@@ -16,9 +23,15 @@ public class CustomerDemoApplication {
     }
 
     @Bean
-    @LoadBalanced
+//    @LoadBalanced  //通过ribbon自动进行负载均衡
     public RestTemplate template(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule ribbonRule(){
+        //自定义规则
+        return new RandomRule();
     }
 
 }
--- distributed-systems-development/spring-cloud-eureka/customer-demo/src/main/java/szu/panda/controller/CustomerController.java
+++ distributed-systems-development/spring-cloud-eureka/customer-demo/src/main/java/szu/panda/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package szu.panda.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +13,17 @@ public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient; //使用原生态ribbon
+
     @GetMapping("index")
     public Object getIndex(){
-        return restTemplate.getForObject("http://HELLOSERVER/",String.class,"");
+//        return restTemplate.getForObject("http://HELLOSERVER/",String.class,"");
+        //自定义ribbon
+        ServiceInstance serviceInstance = loadBalancerClient.choose("hello-server"); //拿到服务实例
+        String ip = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        return restTemplate.getForObject("http://" + ip + ":" + port ,String.class,"");
     }
 
 
